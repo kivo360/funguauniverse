357cffa55aebce4e3ab54c36de2b922352450cd4
Administrator <admin@example.com>
2019-05-20 19:10:20 +0900

    Added main objects. Pushing to pypi

--- funtoolbox/servicetools/store_item.py
+++ funtoolbox/servicetools/store_item.py
@@ -1,20 +1,22 @@
-import random
-import uuid
-
 from featman import Features
+
 
 class StoreItem(object):
     """
         # StoreItem
         ---
-        Allow the user to consistently save and load features with abstractions
+        Save and load an item with dynamic programming
     """
     def __init__(self, *args, **kwargs):
         self.features = Features()
-    
+
+
     def save(self, id, obj):
         # Save the item
         raise NotImplementedError
     
     def load(self, id):
         raise NotImplementedError
+
+
+
